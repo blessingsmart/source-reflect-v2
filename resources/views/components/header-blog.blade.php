293ebb26a02626aab5e6ec8374b124e78df5b601
blessingsmart <bublessingsmart@gmail.com>
2025-11-29 17:16:27 +0100

    removed the signup button

--- resources/views/components/header-blog.blade.php
+++ resources/views/components/header-blog.blade.php
@@ -50,12 +50,12 @@
               Login
             </a>
           </li>
-          <li>
+          {{-- <li>
             <a href="{{route('register')}}" 
                class="bg-gradient-to-r from-green-600 to-emerald-600 text-white px-4 py-2 rounded-lg font-medium text-sm hover:shadow-lg transition-all duration-300 hover:scale-105 {{Route::is('register') ? 'shadow-lg' : ''}}">
               Get Started
             </a>
-          </li>
+          </li> --}}
           @else  
           {{-- Blog Link --}}
           <li>
